d201a7f35bce36391b1d4ab10fda56e82cbdbca6
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-09 16:45:35 +0100

    refreshing

--- variables and simple datatypes.py
+++ variables and simple datatypes.py
@@ -1,9 +1,26 @@
 #print("Hello World")
 
+
 # message = "This is a test message"
 # print(message)
 
-name = "turbo  turttle"
-print(name.title())
-print(name.upper())
-print(name.lower())
+
+# name = "turbo  bee"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+
+# first_name = "turbo"
+# last_name = "bee"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+# print(full_name.upper)
+# print(full_name.lower)
+# print(f"What is that a {full_name}!")
+# print(f"What is that a {full_name.upper()}!")
+# print(f"What is that a {full_name.lower()}!")
+# message = f"What is that a {full_name.lower()}!"
+# print(message)
+
+
