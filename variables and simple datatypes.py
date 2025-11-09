87583f0b5cf00cd5194bc619f711c5d96cc52839
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-09 22:44:44 +0100

    refresher

--- variables and simple datatypes.py
+++ variables and simple datatypes.py
@@ -40,3 +40,14 @@
 # 'nostrach.com'
 
 
+# # Lists
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+# print(bicycles[0])
+# print(bicycles[1])
+# print(bicycles[2])
+# print(bicycles[3])
+# print(bicycles[0].title())
+# print(bicycles[1].title())
+# print(bicycles[2].title())
+# print(bicycles[3].title())
