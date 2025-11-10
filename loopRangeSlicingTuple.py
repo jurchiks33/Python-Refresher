64bc4154b207ee7d31ff9c93af2a220dd5c06c3f
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-10 18:20:15 +0100

    test

--- loopRangeSlicingTuple.py
+++ loopRangeSlicingTuple.py
@@ -7,3 +7,14 @@
 #     print(f"Hello, my name is {name.title()}.")
 
 
+# for value in range(1, 10):
+#     print(value)
+
+# numbers = list(range(1, 5))
+# print(numbers)
+
+
+# even_numbers = list(range(2, 20, 2))
+# print(even_numbers)
+
+
