6e426f0db4dfe4b68662e24cfbbb241fff602102
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-10 18:15:31 +0100

    refresher

---
+++ loopRangeSlicingTuple.py
@@ -0,0 +1,9 @@
+
+
+# names = ['juris', 'erik', 'ronald', 'jon', 'sigrid']
+# for name in names:
+#     print(name)
+#     print(f"{name.title()}, such a great name!")
+#     print(f"Hello, my name is {name.title()}.")
+
+
