27adb3505ea61d86b2d0ac58b162063ff700e45e
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-10 18:27:11 +0100

    refresher

--- loopRangeSlicingTuple.py
+++ loopRangeSlicingTuple.py
@@ -18,3 +18,14 @@
 # print(even_numbers)
 
 
+# squares = []
+# for value in range(1, 11):
+#     square = value ** 2
+#     squares.append(square)
+
+# print(squares)
+
+# squares = [value**2 for value in range(1, 15)]
+# print(squares)
+
+
