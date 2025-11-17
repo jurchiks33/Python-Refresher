e1c2b56b164a335e8da509167bc7b0b4605ecc72
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-17 18:53:52 +0100

    test

--- dictionaries.py
+++ dictionaries.py
@@ -28,10 +28,27 @@
 
 # print(Book_0)
 
+#===============================================================
 
 # book_0 = {'cover': 'hardcover'}
 # print(f"books current cover is {book_0['cover']}")
 # book_0['cover'] = 'softcover'
 # print(f"New cover for the book is {book_0['cover']}")
 
+#==============================================================
+
+# car_0 = {'x_position': 10, 'y_position': 50, 'speed': 20}
+# print(f"original position is {car_0 ['x_position']}")
+
+# if car_0['speed'] == 15:
+#     x_increment = 1
+# elif car_0['speed'] == 20:
+#     x_increment = 5
+# else:
+#     x_increment = 10
+
+# car_0['x_position'] = car_0['x_position'] + x_increment
+# print(f"New cars position is: {car_0['x_position']}")
+
+#=============================================================
 
