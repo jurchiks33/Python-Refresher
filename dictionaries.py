49ccba8ede2d65f041dba38c0ec22a3862e96391
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-18 11:26:48 +0100

    refresher

--- dictionaries.py
+++ dictionaries.py
@@ -124,17 +124,30 @@
 
 #========================================================
 
-aliens = []
+# aliens = []
 
-for alien_number in range(30):
-    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
-    aliens.append(new_alien)
+# for alien_number in range(30):
+#     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+#     aliens.append(new_alien)
 
-for alien in aliens[:5]:
-    print(alien)
-print("...")
+# for alien in aliens[:5]:
+#     print(alien)
+# print("...")
 
-print(f"Total number of aliens: {len(aliens)}")
+# print(f"Total number of aliens: {len(aliens)}")
+
+#=======================================================
+
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese'],
+# }
+
+# print(f"You ordered a {pizza['crust']}-crust pizza"
+#       "with the folloiwng toppings:")
+
+# for topping in pizza['toppings']:
+#         print(f"\t{topping}")
 
 #=======================================================
 
