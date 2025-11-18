4b3dc82034eca165e3617344c065a41d02f6bbf9
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-18 19:59:10 +0100

    refresher

--- dictionaries.py
+++ dictionaries.py
@@ -151,3 +151,17 @@
 
 #=======================================================
 
+# favorite_languages = {
+#     'jen': ['python', 'rust'],
+#     'sarah': ['c'],
+#     'edward': ['rust', 'go'],
+#     'juris': ['python', 'javascript', 'c++'],
+# }
+
+# for name, languages in favorite_languages.items():
+#     print(f"\n{name.title()} 's favorite languages are:")
+#     for language in languages:
+#         print(f"\t{language.title()}")
+
+#======================================================
+
