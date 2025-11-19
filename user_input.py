47a6a378e6f244628b250a3a23012ab83467483f
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-19 10:19:16 +0100

    refresher

--- user_input.py
+++ user_input.py
@@ -11,3 +11,12 @@
 # print(f"\nHello {name}!")
 
 #===============================================================================
+
+# prompt = " If you share your name, we can personalise message you see."
+# prompt += "\nWhat is your name?"
+
+# name = input(prompt)
+# print(f"\nHello, {name}!")
+
+#===============================================================================
+
