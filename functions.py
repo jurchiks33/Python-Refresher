9e60661477315ed81b19593071ad3612c18dfec4
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-20 13:09:08 +0100

    refresher

--- functions.py
+++ functions.py
@@ -27,3 +27,13 @@
 
 #===============================================================================
 
+# def describe_animal(animal_type, pet_name):
+#     """Display information about a pet"""
+#     print(f"I have a {animal_type}!")
+#     print(f"My {animal_type}'s name is {pet_name.title()}")
+
+# describe_animal('hamster', 'combat')
+# describe_animal('dog', 'starky')
+
+#===============================================================================
+
