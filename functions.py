279a0dd946e9f27c1fe743f5f6189aaa53eb0817
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-21 09:24:36 +0100

    refresher

--- functions.py
+++ functions.py
@@ -75,3 +75,19 @@
 
 #===============================================================================
 
+# def formatted_name(first_name, last_name, middle_name=''):
+#     """get formatted name"""
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# musician = formatted_name('jimi', 'hendrix')
+# print(musician)
+
+# musician = formatted_name('john', 'hooker', 'lee')
+# print(musician)
+
+#===============================================================================
+
