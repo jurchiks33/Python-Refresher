56cc6ef1aa38d09d9cf05ad8fa6470c6d949bca5
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-21 09:31:16 +0100

    refresher

--- functions.py
+++ functions.py
@@ -91,3 +91,13 @@
 
 #===============================================================================
 
+# def build_person(first_name, last_name):
+#     """Return information about a person"""
+#     person = {'first': first_name, 'last': last_name}
+#     return person
+
+# musician = build_person('jimi', 'hendrix')
+# print(musician)
+
+#===============================================================================
+
