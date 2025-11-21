a4ff1a6103b5f1cca6c7005afe0403c80876a30e
Jurchiks Jurchiks <jurisslisans@gmail.com>
2025-11-21 09:51:30 +0100

    refresher

--- functions.py
+++ functions.py
@@ -101,3 +101,15 @@
 
 #===============================================================================
 
+# def build_person(first_name, last_name, age=None):
+#     """Dictionary about a person"""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+
+# musician = build_person('jimi', 'hendrix', age=35)
+# print=(musician)
+
+#===============================================================================
+
