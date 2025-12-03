e680f42043c5ff264a4d33a7bcc25ff10d93f771
jurchiks33 <jurisslisans@gmail.com>
2025-12-03 15:49:20 +0100

    refresher

---
+++ test.py
@@ -0,0 +1,16 @@
+
+#===============================================================================
+
+# def get_user_height():
+#     height = input("How tall are you in cm?")
+#     height = int(height)
+
+#     if height >= 180:
+#         print("\nYou are one tall lad")
+#     else:
+#         print("\nNice you still have way to grow")
+
+# get_user_height()
+
+#===============================================================================
+
