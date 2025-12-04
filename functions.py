fe57c624682d07a6dcddf4d2cac5baecc86f6780
jurchiks33 <jurisslisans@gmail.com>
2025-12-04 20:46:23 +0100

    refresher

--- functions.py
+++ functions.py
@@ -191,16 +191,23 @@
 
 #===============================================================================
 
-def print_models(unprinted_designs, completed_designs):
-    """Simulate printing designs until none is left"""
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"printing model: {current_design}")
-        completed_models.append(current_design)
+# def print_models(unprinted_designs, completed_designs):
+#     """Simulate printing designs until none is left"""
+#     while unprinted_designs:
+#         current_design = unprinted_designs.pop()
+#         print(f"printing model: {current_design}")
+#         completed_models.append(current_design)
 
-def show_completed_models(completed_models):
-    """Show all models that are printed"""
-    print("\nThe following models have beed printed: ")
-    for completed_model in completed_models:
-        print(completed_model)
+# def show_completed_models(completed_models):
+#     """Show all models that are printed"""
+#     print("\nThe following models have beed printed: ")
+#     for completed_model in completed_models:
+#         print(completed_model)
 
+# unprinted_designs = ['phone case', 'robot pendant', 'tree']
+# completed_models = []
+
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
+
+#===============================================================================
