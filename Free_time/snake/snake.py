345631a6d0dd3eadf908e86f2b9aa99ebab41720
jurchiks33 <jurisslisans@gmail.com>
2026-01-07 17:09:28 +0100

    snake game

---
+++ Free_time/snake/snake.py
@@ -0,0 +1,13 @@
+# snake game
+
+import curses
+import random
+
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.nodelay(True)
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_RED, -1) #Apple color
+
+    
